62d867f75263f8d41d6a24627025f709352e2281
Gniewk0 <woj.soltysiak@gmail.com>
2022-03-18 10:40:19 +0100

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,67 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="jumbotron">
+      <div class="d-flex justify-content-center">
+       <form @submit.prevent>
+        <div class="form-group">
+          <label for="text">Text</label>
+          <input type="text" class="form-control" id="text" v-model="text">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+        </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Text field</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"  v-model="textarea"></textarea>
+        </div>
+        <button type="submit" class="btn btn-secondary" @click="submit">Submit</button>
+
+        <div class="alert alert-success mt-4" role="alert" v-if="success">
+          Success!
+        </div>
+
+        <div class="alert alert-danger mt-4" role="alert" v-if="error">
+          Error!
+        </div>
+
+      </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      text: '',
+      password: '',
+      textarea: '',
+      error: false,
+      success: true
+    }
+  },
   props: {
     msg: String
+  },
+  methods: {
+    submit() {
+      this.axios.post('https://testoweee.free.beeceptor.com', {
+          text: this.text,
+          password: this.password,
+          textarea: this.textarea,
+        })
+        .then(function (response) {
+          this.success = true;
+          console.log(response);
+        })
+        .catch(function (error) {
+          this.error = true;
+          console.log(error);
+        });
+    }
   }
 }
 </script>
